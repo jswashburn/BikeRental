485a2b7e826bb4e744d4e314bc52c78a9ac69a64
jswashburn <77169258+jswashburn@users.noreply.github.com>
2021-04-29 08:28:53 -0400

    Delete old folder

---
+++ BikeRental/Models/Repositories/IRepositoryAsync.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BikeRental.Models.Repositories
+{
+    public interface IRepositoryAsync<T> where T : IEntity
+    {
+        Task<IEnumerable<T>> GetAsync(string controller);
+        Task<T> GetAsync(int id, string controller);
+        Task<T> InsertAsync(T item, string controller);
+        Task<T> DeleteAsync(int id, string controller);
+        Task<T> UpdateAsync(T item, string controller);
+    }
+}
