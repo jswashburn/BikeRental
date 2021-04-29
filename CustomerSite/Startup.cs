6854f87bae8b004a6678fc465fdb8b5ea02fe3cc
jswashburn <77169258+jswashburn@users.noreply.github.com>
2021-04-29 10:51:28 -0400

    Add customer site

--- CustomerSite/Startup.cs
+++ CustomerSite/Startup.cs
@@ -1,14 +1,11 @@
+using BikeRentalApi.Models.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Threading.Tasks;
+using System.Net.Http.Headers;
 
 namespace CustomerSite
 {
@@ -24,8 +21,17 @@ namespace CustomerSite
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHttpClient("Customer", client =>
+            {
+                client.BaseAddress = new Uri("https://localhost:44375/api/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept
+                    .Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            });
+
             services.AddControllersWithViews();
-            services.AddSingleton<HttpClient>();
+
+            services.AddScoped(typeof(IRepositoryAsync<>), typeof(ApiRepository<>));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
