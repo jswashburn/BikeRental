dd4b53b6020533bc8c64f45a0fdd3e917d9e385c
jswashburn <77169258+jswashburn@users.noreply.github.com>
2021-04-29 16:57:00 -0400

    Add BikeRentalRoute class to hold api route string consts

---
+++ CustomerSite/BikeRentalRoute.cs
@@ -0,0 +1,11 @@
+﻿namespace CustomerSite
+{
+    public class BikeRentalRoute
+    {
+        public const string Bikes = "bikes";
+        public const string Customers = "customers";
+        public const string Reservations = "reservations";
+        public const string BikeStores = "bikestores";
+        public const string Employees = "employees";
+    }
+}
