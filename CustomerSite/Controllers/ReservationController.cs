5aa69867c3f4361aa85000c2bbf78e200cc7c9fc
jswashburn <77169258+jswashburn@users.noreply.github.com>
2021-04-29 13:57:28 -0400

    Add reservation controller

---
+++ CustomerSite/Controllers/ReservationController.cs
@@ -0,0 +1,32 @@
+﻿using BikeRentalApi.Models;
+using BikeRentalApi.Models.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CustomerSite.Controllers
+{
+    public class ReservationController : Controller
+    {
+        readonly IRepositoryAsync<Reservation> _reservationsRepo;
+
+        const string _routeReservations = "reservations";
+
+        public ReservationController(IRepositoryAsync<Reservation> reservations)
+        {
+            _reservationsRepo = reservations;
+        }
+
+        public IActionResult Index(Customer customer)
+        {
+            return View(customer);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateReservation(Reservation reservation)
+        {
+            Reservation confirmed = await _reservationsRepo
+                .InsertAsync(reservation, _routeReservations);
+            return View("ConfirmReservation", confirmed);
+        }
+    }
+}
