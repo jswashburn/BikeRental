f9ad7a8c9b03eee2c59bb81eab46040f71082350
jswashburn <77169258+jswashburn@users.noreply.github.com>
2021-05-01 13:36:44 -0400

    bring in employee site

---
+++ EmployeeSite/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using BikeRentalApi.Models;
+using BikeRentalApi.Models.Repositories;
+using EmployeeSite.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        readonly IRepositoryAsync<Bike> _bikesRepo;
+        const string _route = "bikes";
+
+        public HomeController(IRepositoryAsync<Bike> bikes)
+        {
+            _bikesRepo = bikes;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<Bike> bikes = await _bikesRepo.GetAsync(_route);
+            return View(bikes);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ EmployeeSite/Startup.cs
@@ -0,0 +1,62 @@
+using BikeRentalApi.Models.Repositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Net.Http.Headers;
+
+namespace EmployeeSite
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHttpClient("Employee", client =>
+            {
+                client.BaseAddress = new Uri("https://localhost:44323/api/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept
+                    .Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            });
+            services.AddControllersWithViews();
+            services.AddScoped(typeof(IRepositoryAsync<>), typeof(ApiRepository<>));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
