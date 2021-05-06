b881a98865b419098134e59ed311d32e0b3a98e3
jswashburn <77169258+jswashburn@users.noreply.github.com>
2021-05-06 09:04:48 -0400

    add availability check to reserved bikes

---
+++ CustomerSite/ViewComponents/AvailabilityCheckViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BikeRentalApi.Models;
+using BikeRentalApi.Models.Repositories;
+using System.Threading.Tasks;
+using BikeRentalApi.Repositories.Extensions;
+
+namespace CustomerSite.ViewComponents
+{
+    public class AvailabilityCheckViewComponent : ViewComponent
+    {
+        readonly IRepositoryAsync<Reservation> _reservationsRepo;
+
+        public AvailabilityCheckViewComponent(IRepositoryAsync<Reservation> reservations)
+        {
+            _reservationsRepo = reservations;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int bikeId)
+        {
+            Reservation reservation = await _reservationsRepo.GetByBikeIdAsync(bikeId);
+            return View(reservation);
+        }
+    }
+}
