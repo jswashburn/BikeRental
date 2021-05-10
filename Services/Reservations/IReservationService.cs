2adf35d78c5d2739d89cb1ff5fa0b58546bd5e65
jswashburn <77169258+jswashburn@users.noreply.github.com>
2021-05-10 16:42:33 -0400

    add reservation service files

---
+++ Services/Reservations/IReservationService.cs
@@ -0,0 +1,11 @@
+﻿using BikeRentalApi.Models;
+using System.Threading.Tasks;
+
+namespace CustomerSite.Services
+{
+    public interface IReservationService
+    {
+        Task<Reservation> CreateReservation(Customer customer, int bikeId, int daysRequested);
+        Task<Bike> GetBikeFromId(int bikeId);
+    }
+}
