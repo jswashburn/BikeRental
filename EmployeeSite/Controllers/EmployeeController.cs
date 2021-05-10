82db024011624a5677b8a4f56a346499e2e0014a
Maks <maxkarepov@gmail.com>
2021-05-10 09:06:32 -0400

    changes

--- EmployeeSite/Controllers/EmployeeController.cs
+++ EmployeeSite/Controllers/EmployeeController.cs
@@ -35,6 +35,10 @@ namespace EmployeeSite.Controllers
         {
             return View();
         }
+        public IActionResult Edit()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
