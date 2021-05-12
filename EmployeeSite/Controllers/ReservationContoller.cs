72587e51e777ac0bdcc9e6d36ffb8eb925c9499f
jswashburn <77169258+jswashburn@users.noreply.github.com>
2021-05-12 12:23:20 -0400

    making use of reservation service so bikes are properly updated when reservations are deleted and created

--- EmployeeSite/Controllers/ReservationContoller.cs
+++ EmployeeSite/Controllers/ReservationContoller.cs
@@ -3,6 +3,7 @@ using EmployeeSite.Models;
 using Microsoft.AspNetCore.Mvc;
 using Services;
 using Services.Repositories;
+using Services.Reservations;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -12,43 +13,38 @@ namespace EmployeeSite.Controllers
 {
     public class ReservationController : Controller
     {
-        readonly IRepositoryAsync<Reservation> _resRepo;
-        readonly IRepositoryAsync<Customer> _custRepo;
-        readonly IRepositoryAsync<Bike> _bikeRepo;
+        readonly IReservationService _reservationService;
 
-        public ReservationController(IRepositoryAsync<Reservation> res, IRepositoryAsync<Customer> cust,
-            IRepositoryAsync<Bike> bike)
+        public ReservationController(IReservationService reservationService)
         {
-            _resRepo = res;
-            _custRepo = cust;
-            _bikeRepo = bike;
+            _reservationService = reservationService;
         }
+
         [HttpGet]
         public async Task<IActionResult> Index()
         {
-            IEnumerable<Reservation> res = await _resRepo.GetAsync(BikeRentalRoute.Reservations);
-            IEnumerable<Bike> bike = await _bikeRepo.GetAsync(BikeRentalRoute.Bikes);
-            IEnumerable<Customer> cust = await _custRepo.GetAsync(BikeRentalRoute.Customers);
-            foreach (Reservation r in res)
-            {
-                r.Customer = cust.FirstOrDefault(p => r.CustomerId == p.Id);
-                r.Bike = bike.FirstOrDefault(p => p.Id == r.BikeId);
-            }
-            return View(res);
+            IEnumerable<Reservation> reservations = await _reservationService
+                .GetReservationsAsync();
+
+            return View(reservations);
         }
+
         [HttpGet]
         public IActionResult Privacy()
         {
             return View();
         }
+
         public IActionResult Edit()
         {
             return View();
         }
+
         public IActionResult Create()
         {
             return View();
         }
+
         [HttpPost]
         public async Task<IActionResult> Create([Bind("CustomerId,BikeId,CurrentStoreId,Archive,DateReserved,DateDue,DateReturned,GrandTotal")] Reservation res)
         {
@@ -57,24 +53,20 @@ namespace EmployeeSite.Controllers
                 TempData["InvalidSubmit"] = true;
                 return View(nameof(Index), res);
             }
-            res = await _resRepo.InsertAsync(res, BikeRentalRoute.Reservations);
+            res = await _reservationService.CreateReservationAsync(res);
             return View("Bike created", res);
         }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
         public async Task<IActionResult> Delete(int id)
         {
-            Reservation res = await _resRepo.GetAsync(id, BikeRentalRoute.Reservations);
-            if (res == null)
-                return NotFound();
-
-            await _resRepo.DeleteAsync(id, BikeRentalRoute.Reservations);
-
+            await _reservationService.DeleteReservationAsync(id);
             return RedirectToAction(nameof(Index));
-
         }
     }
 }
